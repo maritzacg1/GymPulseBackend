4b82e35d3a49fc964caba7eca44708512555c120
maritzacg1 <kchalen14@gmail.com>
2026-07-23 14:32:11 -0500

    Nuevo cambio

--- src/routes/historial.routes.js
+++ src/routes/historial.routes.js
@@ -1,13 +1,31 @@
 import { Router } from 'express';
+
 import {
   getHistorial,
-  registrarHistorial
+  registrarHistorialApi
 } from '../controladores/historialCtrl.js';
+
 import { verifyToken } from '../jwt/verifyToken.js';
+
 
 const router = Router();
 
-router.get('/historial', verifyToken, getHistorial);
-router.post('/historial', verifyToken, registrarHistorial);
+
+
+router.get(
+  '/historial',
+  verifyToken,
+  getHistorial
+);
+
+
+
+router.post(
+  '/historial',
+  verifyToken,
+  registrarHistorialApi
+);
+
+
 
 export default router;
